934533889ef136be7611b18c17761fba437ebc2c
Michael Nelson <michael.nelson@gov.bc.ca>
2022-06-07 11:23:46 -0700

    Add vector basemap config

--- config/site/config.js
+++ config/site/config.js
@@ -1,3 +1,12 @@
 window.TLINK_CONFIG = {
-    "regionalRoadsApiUrl": "https://devapi.regionalroads.com/simplewfs/"
+  "regionalRoadsApiUrl": "https://devapi.regionalroads.com/simplewfs/",
+  "baseMapNames": new Map([
+    ["Topographic", "ArcGIS:Topographic"],
+    ["Streets", "OSM:Standard"],
+    ["Imagery", "ArcGIS:Imagery"],
+    ["StamenTonerLight", "StamenTonerLight"],
+    ["DarkGray", "ArcGIS:DarkGray"],
+    ["Gray", "ArcGIS:LightGray"]
+  ]),
+  "esriApiKey": "<your-api-key>"
 }
