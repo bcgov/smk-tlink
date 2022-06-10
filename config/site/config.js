bc6a4f0e9082ca2f4f635cd515ba4a1b5b4a50b7
Michael Nelson <michael.nelson@gov.bc.ca>
2022-06-10 14:42:55 -0700

    Moved ESRI style names into viewer basemap object

--- config/site/config.js
+++ config/site/config.js
@@ -1,12 +1,4 @@
 window.TLINK_CONFIG = {
   "regionalRoadsApiUrl": "https://devapi.regionalroads.com/simplewfs/",
-  "baseMapNames": new Map([
-    ["Topographic", "ArcGIS:Topographic"],
-    ["Streets", "OSM:Standard"],
-    ["Imagery", "ArcGIS:Imagery"],
-    ["StamenTonerLight", "StamenTonerLight"],
-    ["DarkGray", "ArcGIS:DarkGray"],
-    ["Gray", "ArcGIS:LightGray"]
-  ]),
   "esriApiKey": "<your-api-key>"
 }
